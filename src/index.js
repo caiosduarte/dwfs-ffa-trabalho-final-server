987ae3563e9d47ef94cb7ebd451b5aea6e94569d
Caio <caiosduarte@yahoo.com.br>
2020-10-11 01:50:39 -0300

    alterado o script para produçao

--- src/index.js
+++ src/index.js
@@ -6,10 +6,25 @@ const path = require("path");
 //const cors = require("cors");
 const jwt = require("jsonwebtoken");
 
+
+
 // Importando JSON-Server
 const jsonServer = require("json-server");
 
 
+//import sslRedirect from 'heroku-ssl-redirect';
+// enable ssl redirect
+//app.use(sslRedirect());
+//app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+const validateUser = require("./validate_user.js");
+
+const SECRET_JWT_KEY = "SECRETJWTKEY";
+const port = process.env.PORT || 8080;
+
+/*
 app.use(function (req, res, next) {
   if (req.header('x-forwarded-proto') === 'http') {
     res.redirect(301, 'https://' + req.hostname + req.url);
@@ -17,18 +32,8 @@ app.use(function (req, res, next) {
   }
   next()
 });
+*/
 
-
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-
-const validateUser = require("./validate_user.js");
-
-const SECRET_JWT_KEY = "SECRETJWTKEY";
-
-const port = process.env.PORT || 8080;
-
-//app.use(cors());
 app.use("*", (req, res, next) => {
   console.log(req.query, req.body);
   next();
